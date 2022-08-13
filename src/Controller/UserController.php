7bf41281ebcc4f7a023ebc80e5dee65de5d38d3a
massi9106 <massi9106.ait@gmail.com>
2022-08-13 14:54:07 +0200

    ⚡  fixing  bad code for hashing password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -61,20 +61,39 @@ class UserController extends AbstractController
         ]);
     }
 
+    /**
+     * This controller allow us to modify the passwordUs
+     * @param User $user
+     * @param Request $request
+     * @param UserPasswordHasherInterface $hasher
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+
     #[Route('/utilisateur/edit-mdp/{id}', name: 'app_user_edit_password', methods: ['GET', 'POST'])]
 
-    public function editPassword(User $user, Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $manager): Response
-    {
+    public function editPassword(
+        User $user,
+        Request $request,
+        UserPasswordHasherInterface $hasher,
+        EntityManagerInterface $manager
+    ): Response {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_security_login');
+        }
+
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('app_recipe');
+        }
+
+
         $form = $this->createForm(UserPasswordType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
-
-                $user->setPassword(
-                    $hasher->hashPassword(
-                        $user,
-                        $form->getData()['newPassword']
-                    )
+                $user->setcreatedAt(new \DateTimeImmutable());
+                $user->setPlainPassword(
+                    $form->getData()['newPassword']
                 );
 
                 $manager->persist($user);
--- src/EntityListener/UserListener.php
+++ src/EntityListener/UserListener.php
@@ -18,7 +18,10 @@ class UserListener
         $this->encodePassword($user);
     }
 
-
+    public function preUpdate(User $user)
+    {
+        $this->encodePassword($user);
+    }
     /**
      * Encode password based on plainPassword
      *
