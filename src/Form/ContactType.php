403d78d234870954736082a444349462fb87f92e
massi9106 <massi9106.ait@gmail.com>
2022-08-15 22:59:05 +0200

    ✨  Add  Recaptcha  v3  to contact  form .

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,13 +4,15 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 class ContactType extends AbstractType
 {
@@ -71,12 +73,19 @@ class ContactType extends AbstractType
                     new Assert\NotBlank()
                 ],
             ])
+
             ->add('submit', SubmitType::class, [
 
                 'attr' => [
                     'class' => 'btn btn-primary mt-4 mb-4'
                 ],
                 'label' => 'Envoyer',
+
+            ])
+
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
 
             ]);
     }
